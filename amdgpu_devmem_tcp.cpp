262d3fe7fa98fb42ea885708802d57b26e95ce9c
Taehee Yoo <ap420073@gmail.com>
2025-05-02 05:24:35 +0000

    fix style
    Signed-off-by: Taehee Yoo <ap420073@gmail.com>

--- amdgpu_devmem_tcp.cpp
+++ amdgpu_devmem_tcp.cpp
@@ -1,43 +1,41 @@
 /*
-Copyright (c) 2015-2016 Advanced Micro Devices, Inc. All rights reserved.
+   Copyright (c) 2015-2016 Advanced Micro Devices, Inc. All rights reserved.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+ */
 #include <assert.h>
-#include <stdio.h>
 #include <algorithm>
 #include <stdlib.h>
-#include<iostream>
-#include<unistd.h>
-#include "hip/hip_runtime.h"
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
 #include <hsa/hsa.h>
+#include "hip/hip_runtime.h"
 #include "/opt/rocm-6.2.0/include/hsa/hsa_ext_finalize.h"
 #include "/opt/rocm-6.2.0/include/hsa/hsa_ext_image.h"
 #include "/opt/rocm-6.2.0/include/hsa/hsa_ext_amd.h"
-
 
 #ifdef NDEBUG
 #define HIP_ASSERT(x) x
 #else
 #define HIP_ASSERT(x) (assert((x)==hipSuccess))
 #endif
-
 
 #define WIDTH     1024
 #define HEIGHT    1024
@@ -49,124 +47,114 @@ THE SOFTWARE.
 #define THREADS_PER_BLOCK_Z  1
 
 __global__ void 
-vectoradd_float(float* __restrict__ a, const float* __restrict__ b, const float* __restrict__ c, int width, int height) 
+vectoradd_float(float* __restrict__ a, const float* __restrict__ b,
+		const float* __restrict__ c, int width, int height) 
 
-  {
- 
-      int x = hipBlockDim_x * hipBlockIdx_x + hipThreadIdx_x;
-      int y = hipBlockDim_y * hipBlockIdx_y + hipThreadIdx_y;
+{
+	int x = hipBlockDim_x * hipBlockIdx_x + hipThreadIdx_x;
+	int y = hipBlockDim_y * hipBlockIdx_y + hipThreadIdx_y;
 
-      int i = y * width + x;
-      if ( i < (width * height)) {
-        a[i] = b[i] + c[i];
-      }
-
-
-
-  }
+	int i = y * width + x;
+	if ( i < (width * height)) {
+		a[i] = b[i] + c[i];
+	}
+}
 
 #if 0
 __kernel__ void vectoradd_float(float* a, const float* b, const float* c, int width, int height) {
 
-  
-  int x = blockDimX * blockIdx.x + threadIdx.x;
-  int y = blockDimY * blockIdy.y + threadIdx.y;
 
-  int i = y * width + x;
-  if ( i < (width * height)) {
-    a[i] = b[i] + c[i];
-  }
+	int x = blockDimX * blockIdx.x + threadIdx.x;
+	int y = blockDimY * blockIdy.y + threadIdx.y;
+
+	int i = y * width + x;
+	if ( i < (width * height)) {
+		a[i] = b[i] + c[i];
+	}
 }
 #endif
 
 using namespace std;
+int main()
+{
+	int dmabuf_fd = -1;
+	hsa_status_t err;
+	uint64_t offset;
+	float* deviceA;
+	float* deviceB;
+	float* deviceC;
+	float* hostA;
+	float* hostB;
+	float* hostC;
+	int errors;
+	int i;
 
-int main() {
-  
-  float* hostA;
-  float* hostB;
-  float* hostC;
-
-  float* deviceA;
-  float* deviceB;
-  float* deviceC;
-
-  hipDeviceProp_t devProp;
-  hipGetDeviceProperties(&devProp, 0);
-  cout << " System minor " << devProp.minor << endl;
-  cout << " System major " << devProp.major << endl;
-  cout << " agent prop name " << devProp.name << endl;
-
-
-
-  cout << "hip Device prop succeeded " << endl ;
-
-
-  int i;
-  int errors;
-
-  hostA = (float*)malloc(NUM * sizeof(float));
-  hostB = (float*)malloc(NUM * sizeof(float));
-  hostC = (float*)malloc(NUM * sizeof(float));
-  
-  // initialize the input data
-  for (i = 0; i < NUM; i++) {
-    hostB[i] = (float)i;
-    hostC[i] = (float)i*100.0f;
-  }
-  
-  HIP_ASSERT(hipMalloc((void**)&deviceA, NUM * sizeof(float)));
-  HIP_ASSERT(hipMalloc((void**)&deviceB, NUM * sizeof(float)));
-  HIP_ASSERT(hipMalloc((void**)&deviceC, NUM * sizeof(float)));
-  
-  HIP_ASSERT(hipMemcpy(deviceB, hostB, NUM*sizeof(float), hipMemcpyHostToDevice));
-  HIP_ASSERT(hipMemcpy(deviceC, hostC, NUM*sizeof(float), hipMemcpyHostToDevice));
+	hipDeviceProp_t devProp;
+	hipGetDeviceProperties(&devProp, 0);
+	cout << " System minor " << devProp.minor << endl;
+	cout << " System major " << devProp.major << endl;
+	cout << " agent prop name " << devProp.name << endl;
 
 
-  hipLaunchKernelGGL(vectoradd_float, 
-                  dim3(WIDTH/THREADS_PER_BLOCK_X, HEIGHT/THREADS_PER_BLOCK_Y),
-                  dim3(THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y),
-                  0, 0,
-                  deviceA ,deviceB ,deviceC ,WIDTH ,HEIGHT);
 
-  int dmabuf_fd = -1;
-  uint64_t offset;
-  hsa_status_t err = hsa_amd_portable_export_dmabuf((void *)deviceA,
-						    NUM * sizeof(float),
-						    &dmabuf_fd,
-						    &offset);
+	cout << "hip Device prop succeeded " << endl ;
 
-  if (err != HSA_STATUS_SUCCESS) {
-	  fprintf(stderr, "dmabuf export failed!\n");
-  } else {
-	  printf("dmabuf export succeeded!\n");
-  }
+	hostA = (float*)malloc(NUM * sizeof(float));
+	hostB = (float*)malloc(NUM * sizeof(float));
+	hostC = (float*)malloc(NUM * sizeof(float));
 
-  HIP_ASSERT(hipMemcpy(hostA, deviceA, NUM*sizeof(float), hipMemcpyDeviceToHost));
+	// initialize the input data
+	for (i = 0; i < NUM; i++) {
+		hostB[i] = (float)i;
+		hostC[i] = (float)i*100.0f;
+	}
 
-  sleep(100000);
-  // verify the results
-  errors = 0;
-  for (i = 0; i < NUM; i++) {
-    if (hostA[i] != (hostB[i] + hostC[i])) {
-      errors++;
-    }
-  }
-  if (errors!=0) {
-    printf("FAILED: %d errors\n",errors);
-  } else {
-      printf ("PASSED!\n");
-  }
+	HIP_ASSERT(hipMalloc((void**)&deviceA, NUM * sizeof(float)));
+	HIP_ASSERT(hipMalloc((void**)&deviceB, NUM * sizeof(float)));
+	HIP_ASSERT(hipMalloc((void**)&deviceC, NUM * sizeof(float)));
 
-  HIP_ASSERT(hipFree(deviceA));
-  HIP_ASSERT(hipFree(deviceB));
-  HIP_ASSERT(hipFree(deviceC));
+	HIP_ASSERT(hipMemcpy(deviceB, hostB, NUM*sizeof(float),
+			     hipMemcpyHostToDevice));
+	HIP_ASSERT(hipMemcpy(deviceC, hostC, NUM*sizeof(float),
+			     hipMemcpyHostToDevice));
 
-  free(hostA);
-  free(hostB);
-  free(hostC);
+	hipLaunchKernelGGL(vectoradd_float, 
+			   dim3(WIDTH / THREADS_PER_BLOCK_X,
+				HEIGHT / THREADS_PER_BLOCK_Y),
+			   dim3(THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y),
+			   0, 0,
+			   deviceA ,deviceB ,deviceC ,WIDTH ,HEIGHT);
 
-  //hipResetDefaultAccelerator();
+	err = hsa_amd_portable_export_dmabuf((void *)deviceA,
+					     NUM * sizeof(float),
+					     &dmabuf_fd,
+					     &offset);
+	if (err != HSA_STATUS_SUCCESS)
+		fprintf(stderr, "dmabuf export failed!\n");
+	else
+		printf("dmabuf export succeeded!\n");
 
-  return errors;
+	HIP_ASSERT(hipMemcpy(hostA, deviceA, NUM*sizeof(float),
+			     hipMemcpyDeviceToHost));
+	errors = 0;
+	for (i = 0; i < NUM; i++)
+		if (hostA[i] != (hostB[i] + hostC[i]))
+			errors++;
+
+	if (errors!=0)
+		printf("FAILED: %d errors\n",errors);
+	else
+		printf ("PASSED!\n");
+
+	HIP_ASSERT(hipFree(deviceA));
+	HIP_ASSERT(hipFree(deviceB));
+	HIP_ASSERT(hipFree(deviceC));
+
+	free(hostA);
+	free(hostB);
+	free(hostC);
+
+	//hipResetDefaultAccelerator();
+
+	return errors;
 }
